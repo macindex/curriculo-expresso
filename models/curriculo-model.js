72e288906c62148f8e6f415d56e7f7eff84943b1
marcio lira <machollanda@gmail.com>
2020-11-18 23:41:33 -0300

    organizando dados da pagina

---
+++ models/curriculo-model.js
@@ -0,0 +1,26 @@
+const data = {
+    
+title: 'Meu currículo',
+name: 'Claudio Holanda',
+profession: 'Software Engineer',
+description: 'Experiência em desenvolvimento de single page applications com JavaScript e frameworks JavaScript, desenvolvimento em projetos pessoais com Angular e React, package managers, transpilers (como Babel), pre processadores CSS (Sass e Stylus), task managers, arquitetura CSS (como BEM e SMACSS), Git, SEO, acessibilidade e usabilidade.',
+experience: [{
+    company: 'ASSARTE',
+    office: 'Assistente administrativo',
+    description: 'Trabalhos administrativo como conferência de mercadorias, recebimento e pagamentos, gerencia de mídias sociais'
+},
+{
+    company: 'CSU Cardsystem',
+    office: 'Atendimento back-office',
+    description: 'Atendimento e suporte de demandas externas de serviço'
+}],
+    education: [{
+    institution: 'FBV Wyden',
+    description: 'Sobrevivência nas ruas'
+}],
+skills: ['backend', 'frontend', 'infra', 'mobile']
+}
+
+
+
+module.exports = data;

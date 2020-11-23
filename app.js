ea761faea24a608bb95576c41a27129afbd5d4a5
marcio lira <machollanda@gmail.com>
2020-11-22 23:45:11 -0300

    manage error

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+const createError = require('http-errors');
+
 const path = require('path');
 const CurriculoController = require('./controllers/curriculo-controller');
 
 // Setup view engine
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,5 +28,18 @@ app.listen(port, err => {
     console.log(`Server is listening on ${port}`);
 });
 
+// 404
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 
